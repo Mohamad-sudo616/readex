033edc704d2e72996df35f3b45f2eb2b8030f8a5
Mohamad-sudo616 <mohamadamini4884@gmail.com>
2026-07-01 17:53:02 +0330

    phase 8  have completd

--- backend/apps/books/admin.py
+++ backend/apps/books/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "created_at")
+    prepopulated_fields = {"slug": ("title",)}
+    search_fields = ("title", "author")
+    list_filter = ("created_at",)
