f04254b14a5304881fd3fd506bdbca8d6a559a39
Mohamad-sudo616 <mohamadamini4884@gmail.com>
2026-07-11 17:56:53 +0330

    phase 12.3 and 13  have completd

--- backend/apps/books/urls.py
+++ backend/apps/books/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from .views import book_list
+from .views import book_list, book_detail
 
 app_name = "books"
 
 urlpatterns = [
     path("", book_list, name="list"),
+    path("<slug:slug>/", book_detail, name="detail"),
 ]
--- backend/apps/books/views.py
+++ backend/apps/books/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+
 
 from .models import Book
 
@@ -13,5 +15,18 @@ def book_list(request):
         "books/book_list.html",
         {
             "books": books,
+        },
+    )
+
+
+@login_required
+def book_detail(request, slug):
+    book = get_object_or_404(Book, slug=slug)
+
+    return render(
+        request,
+        "books/book_detail.html",
+        {
+            "book": book,
         },
     )
